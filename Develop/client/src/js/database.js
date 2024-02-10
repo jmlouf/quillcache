c42d2cae25109817ea3d389b37bf60d981616037
joem3847 <joem3847@gmail.com>
2024-02-10 09:16:33 -0600

    Added database configuration.

--- Develop/client/src/js/database.js
+++ Develop/client/src/js/database.js
@@ -1,21 +1,63 @@
 import { openDB } from "idb";
 
 const initdb = async () =>
-  openDB("jate", 1, {
+  // Create new database (using version 1 of database).
+  openDB("quillcache", 1, {
+    // Add database schema if not already inititalized.
     upgrade(db) {
-      if (db.objectStoreNames.contains("jate")) {
-        console.log("jate database already exists");
+      if (db.objectStoreNames.contains("quillcache")) {
+        console.log("quillcache database already exists");
         return;
       }
-      db.createObjectStore("jate", { keyPath: "id", autoIncrement: true });
-      console.log("jate database created");
+      // Create new object store for data and give it key name "id" which increments automatically.
+      db.createObjectStore("quillcache", {
+        keyPath: "id",
+        autoIncrement: true,
+      });
+      console.log("quillcache database created");
     },
   });
 
-// TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error("putDb not implemented");
+// Export function to PUT to database.
+export const putDb = async (data) => {
+  console.log("PUT to database.");
 
-// TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error("getDb not implemented");
+  // Connect with database and version to use.
+  const quillcacheDB = await openDB("quillcache", 1);
+
+  // Create new transaction and specify database and data privileges.
+  const tx = quillcacheDB.transaction("quillcache", "readwrite");
+
+  // Open desired object store.
+  const store = tx.objectStore("quillcache");
+
+  // Use .put() method to create structured clone of value.
+  const request = store.put({ data: data });
+
+  // Confirmation of request.
+  const result = await request;
+  console.log("Data saved to database.", result);
+};
+
+// Export function to GET from database.
+export const getDb = async () => {
+  console.log("GET from database.");
+
+  // Connect with database and version to use.
+  const quillcacheDB = await openDB("quillcache", 1);
+
+  // Create new transaction and specify database and data privileges.
+  const tx = quillcacheDB.transaction("quillcache", "readonly");
+
+  // Open desired object store.
+  const store = tx.objectStore("quillcache");
+
+  // Use .put() method to create structured clone of value.
+  const request = store.getAll();
+
+  // Confirmation of request.
+  const result = await request;
+  console.log("Data saved to database.", result);
+};
 
 initdb();
